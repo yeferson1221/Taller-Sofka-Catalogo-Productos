0dd02d1581b1563d360056fd7656bd00bbe6ed07
yeferson1221 <alejandro.yandd@gmail.com>
2022-03-30 22:10:11 -0500

    creando backend productos version 1.0

---
+++ catalogo-productos-springboot/src/main/java/com/productos/categoria/CatalogoProductosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.productos.categoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProductosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProductosSpringbootApplication.class, args);
+	}
+
+}
---
+++ catalogo-productos-springboot/src/test/java/com/productos/categoria/CatalogoProductosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productos.categoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoProductosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
